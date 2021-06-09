141ddd7348dd5b56410d643a2ac2ad655506965b
Neifi Alcantara <neifi@MacBook-Pro.home>
2021-06-09 15:10:39 +0200

    First commit

---
+++ src/test/java/es/neifi/myfinance/MyfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.neifi.myfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
